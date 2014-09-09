cf740fc586aaf88745ea8ff24f601cd4eeec96b4
Roman Pushkin <roman.pushkin@gmail.com>
2014-09-09 23:10:50 +0400

    Init Barbers table on configure

--- app.rb
+++ app.rb
@@ -3,6 +3,20 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def is_barber_exists? db, name
+	db.execute('select * from Barbers where name=?', [name]).length > 0
+end
+
+def seed_db db, barbers
+
+	barbers.each do |barber|
+		if !is_barber_exists? db, barber
+			db.execute 'insert into Barbers (name) values (?)', [barber]
+		end 
+	end
+
+end
+
 def get_db
 	db = SQLite3::Database.new 'barbershop.db'
 	db.results_as_hash = true
@@ -21,6 +35,15 @@ configure do
 			"barber" TEXT,
 			"color" TEXT
 		)'
+
+	db.execute 'CREATE TABLE IF NOT EXISTS
+		"Barbers"
+		(
+			"id" INTEGER PRIMARY KEY AUTOINCREMENT,
+			"name" TEXT
+		)'
+
+	seed_db db, ['Jessie Pinkman', 'Walter White', 'Gus Fring', 'Mike Ehrmantraut']
 end
 
 get '/' do
